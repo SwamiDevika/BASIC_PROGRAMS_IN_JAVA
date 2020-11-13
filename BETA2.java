cc6fd9665d5ac1698bc636b3df6c53debca9a5a1
saugandhika <74425717+saugandhika@users.noreply.github.com>
2020-11-14 00:47:32 +0530

    Add files via upload

---
+++ BETA2.java
@@ -0,0 +1,26 @@
+package com.firstprogram.abc;
+/**
+ * print the argument command line passing through the length the console
+ * and find the length of command line argument passed
+ * @author manjunath aradhya
+ *@version 15.0
+ *@since yyyy-mm-dd
+ */
+public class BETA2 {
+/**
+ * this is the main method which displays command line arguments and length of strings
+ * @param ramesh is passed as argument 1
+ * @param tendulkar is passed as argument 2 
+ * @param sachin is passed as argument 2 
+ * @return nothing
+ */
+	
+	public static void main(String[] args) {
+		System.out.println(args[1]);
+		System.out.println(args[2]);
+		System.out.println(args[0]);
+		System.out.println(args.length);
+
+	}
+
+}
