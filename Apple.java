ddea972dcf9783d6587687188f2dde23b89f28c2
saugandhika <74425717+saugandhika@users.noreply.github.com>
2020-11-13 11:02:32 -0800

    Update Apple.java

--- Apple.java
+++ Apple.java
@@ -1,6 +1,18 @@
 package com.firstprogram.abc;
-
+/**
+calculating the no of packets needed to store 400 apples with condition that each packet contain only 8 apple without using Division symbol(/)
+creted apple class
+@author saugandhika
+@version 15.0
+@since 14-11-2020
+*/
 public class Apple {
+	/**
+	this method is used to calculate no of packets needed
+	@param count
+	@param apple
+	@param args unused
+	*/
 
 	public static void main(String[] args) 
 	{
