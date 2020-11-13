000cf584357688c914eda271a5c830b785be2621
saugandhika <74425717+saugandhika@users.noreply.github.com>
2020-11-14 00:21:16 +0530

    Add files via upload

---
+++ Apple.java
@@ -0,0 +1,17 @@
+package com.firstprogram.abc;
+
+public class Apple {
+
+	public static void main(String[] args) 
+	{
+		int apple=0;
+	    int count=0;
+		while(apple<400)
+		{
+		apple+=8;
+		++count;
+		}
+		System.out.print("no of packets\t"+count);
+	}
+
+}
