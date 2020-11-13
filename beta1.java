d9b30f91ec61f9a13b98d2d4033544cfca8c0a0f
saugandhika <74425717+saugandhika@users.noreply.github.com>
2020-11-14 00:47:04 +0530

    Add files via upload

---
+++ beta1.java
@@ -0,0 +1,22 @@
+package com.firstprogram.abc;
+/**
+ * Print the desired message
+ * and find the initial length of String args[]
+ * @author hi
+ * @version 15.0
+ * @since YYYY-MM-DD
+ */
+
+public class beta1 {
+	/**
+	 * This is main method using which the desired message is displayed
+	 * and also displays the initial length of Strings args[].
+	 * @param args unused
+	 * @return Nothing
+	 */
+	public static void main(String[] args) {
+		System.out.println("hellow ABC");
+		System.out.println(args.length);
+	}
+
+}
