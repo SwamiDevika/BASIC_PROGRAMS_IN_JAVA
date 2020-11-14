16bcf7d0a5e1c60c0ce7c26922df7c2deaa3b589
saugandhika <74425717+saugandhika@users.noreply.github.com>
2020-11-14 14:16:46 +0530

    Add files via upload

---
+++ PatternMethodTesting.java
@@ -0,0 +1,44 @@
+package com.firstprogram.abc;
+
+public class PatternMethodTesting extends Alphabetwritting {
+
+	public static void main(String[] args) {
+		char c1='S';
+		printPattern(c1);
+		System.out.println("\n");
+		char c2='A';
+       printPattern(c2);
+       System.out.println("\n");
+       char c3='U';
+       printPattern(c3);
+       System.out.println("\n");
+       char c4='G';
+       printPattern(c4);
+       System.out.println("\n");
+       char c5='A';
+       printPattern(c5);
+       System.out.println("\n");
+       char c6='N';
+       printPattern(c6);
+       System.out.println("\n");
+       char c7='A';
+       printPattern(c7);
+       System.out.println("\n");
+       char c8='D';
+       printPattern(c8);
+       System.out.println("\n");
+       char c9='H';
+       printPattern(c9);
+       System.out.println("\n");
+       char c10='I';
+       printPattern(c10);
+       System.out.println("\n");
+       char c11='K';
+       printPattern(c11);
+       System.out.println("\n");
+       char c12='A';
+       printPattern(c12);
+       System.out.println("\n");
+	}
+
+}
